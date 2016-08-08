eeedad1691325103a4470fef565979e3a6934ed8
Leah <leahnpetersen@gmail.com>
2016-08-08 12:37:06 -0700

    working on velocity formula

--- python_scripts/velocity_converter.py
+++ python_scripts/velocity_converter.py
@@ -1,14 +1,43 @@
 # use data file to generate velocity data
+# ms, xAccl, yAccl, zAccl, xGyro, yGyro, zGyro
 
 # create new file for velocity data
 vel_file = open("velocity.dat", 'w')
 
+last_ms = 0
+last_velocity = 0
+first_line = True
+
 with open("test_data.txt", "r") as data_file:
+
     for line in data_file:
-    	# um, generate velocity
     	line_array = line.split(' ')
-    	print line_array[0]
-        # vel_file.write(velocity)
+
+    	# on the first line use ms as "last ms" and don't computer velocity
+    	if first_line == True:
+    		last_ms = int(line_array[0])
+    		first_line = False
+    		continue
+
+    	# number on z acceleration when unit is not moving
+    	gravity = 16000
+
+    	ms = int(line_array[0])
+    	# xAccl = line_array[1]
+    	# yAccl = line_array[2]
+    	zAccl = int(line_array[3])
+    	# xGyro = line_array[4]
+    	# yGyro = line_array[5]
+    	# zGyro = line_array[6]
+
+    	current_velocity = (zAccl - gravity) * (ms - last_ms)
+    	velocity = last_velocity + current_velocity
+
+    	# vel_file.write(velocity)
+    	print velocity
+
+    	last_ms = ms
+    	last_velocity = velocity
 
 data_file.close()
 vel_file.close()
