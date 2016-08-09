d91166a21bb6daee7ba4dbc0d5c2c9b07bcb997a
Leah <leahnpetersen@gmail.com>
2016-08-09 15:18:55 -0700

    lots of test data

--- motion_algorithms/walk-trot-canter.py
+++ motion_algorithms/walk-trot-canter.py
@@ -4,6 +4,7 @@ import sys
 current = 0
 current_sec = 0
 first_line = True
+base_time = 0.0
 
 walking = 0
 troting = 0
@@ -11,36 +12,43 @@ cantering = 0
 
 for line in sys.stdin:
 	split = line.rstrip().split(' ')
-
-	# secs = int(split[0])
-	# z_accel = int(split[3])
-
-	current = int(split[3])
-	current_sec = int(split[0])
+	# print float(split[0])
 
 	if first_line == True:
-		prev = int(split[3])
-		prev_sec = int(split[0])
+		prev_z = float(split[3])
+		prev_sec = float(split[0])
+		base_time = prev_sec
 		first_line = False
 
-	current = int(split[3])
-	current_sec = int(split[0])
+	current_z = float(split[3])
+	current_sec = float(split[0])
+
+	current_z = float(split[3])
+	current_sec = float(split[0])
 
 
 	# if negative g force skip
-	if current < 16384.0:
-		prev = current
+	# if current_z < 16384.0:
+	if current_z < 16384.0:
+		prev_z = current
+		prev_sec = current_sec
+		base_time = current_sec
+		continue
+
+	# check that number is still going up
+	if current_z > prev_z:
+		prev_z = current
 		prev_sec = current_sec
 		continue
 
 
 	# get top value by checking if the value has started to fall
-	if current > prev:
-		prev = current
-		prev_sec = current_sec
-		continue
-
 	# if at this point our current is SMALLER than prev, prev is top of the motion curve
-	if prev < 8192:
+	if prev_z < 8192:
 		motion = 0
+		# how much time has passed 
+		walking += (base_time - current_sec)
+
+
+print walking
 
