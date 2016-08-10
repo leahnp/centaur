eb8f47a48eb9a53754826ac5a5afef7008e4ef2e
Leah <leahnpetersen@gmail.com>
2016-08-10 10:48:20 -0700

    algorithm detecting walk, but also extra noise in the low frequencies of the trot/canter

--- motion_algorithms/walk-trot-canter.py
+++ motion_algorithms/walk-trot-canter.py
@@ -1,5 +1,69 @@
 import sys
 
+# def check_axis_swap(axis):
+# 	if axis == 'positive':
+# 		axis = 'negative'
+# 	else: 
+# 		axis = 'positive'
+# 	return axis
+
+#start MAX/MIN tracking when passes 16k
+axis = 'negative'
+swap = 0
+min = -50000
+min_sec = 0
+max = 50000
+max_sec = 0
+
+for line in sys.stdin:
+	split = line.rstrip().split(' ')
+
+	z = float(split[3])
+
+
+	# walk check
+	# positive
+	if (z <= 16384.0) and (z > 8192):
+		# check_axis_swap(axis)
+		if axis == 'negative':
+			axis = 'positive'
+			swap = True
+		else: 
+			swap = False
+
+		if z > min:
+			min = z
+			min_sec = float(split[0])
+
+		# print 'positive'
+
+		if swap == True:
+			# print min
+			print('%f %d %d' % (min_sec, min, 2))
+			min = -50000
+			min_sec = 0
+
+	elif (z > 16384.0) and (z < 24576):
+		# check_axis_swap(axis)
+		if axis == 'positive':
+			axis = 'negative'
+			swap = True
+		else: 
+			swap = False
+		if z < max:
+			max = z
+			max_sec = float(split[0])
+
+		# print 'negative'
+
+		if swap == True:
+			# print max
+			print('%f %d %d' % (max_sec, max, 2))
+			max = 50000
+			max_sec = 0
+
+	else:
+		print('%s %s %d' % (split[0], split[3], 0))
 
 
 
@@ -7,7 +71,9 @@ import sys
 
 
 
-print walking
+
+
+# print walking
 
 
 # # walk= 0 trot= 1 canter=2
