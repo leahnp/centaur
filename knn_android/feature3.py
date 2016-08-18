7d0cf677097df7034874d25274af10c6b2a41036
Leah <leahnpetersen@gmail.com>
2016-08-17 20:34:53 -0700

    new feature: normalized accel on x axis

--- knn_android/feature3.py
+++ knn_android/feature3.py
@@ -1,8 +1,9 @@
-# feature 1 - mean xacceleration
+# feature 3 - mean normalized x-accel
 
+import math
 import sys
 
-WINDOW_LENGTH = 5.0
+WINDOW_LENGTH = 2.0
 
 window_data = []
 window_total = 0.0
@@ -29,13 +30,15 @@ def expire_data(window_end):
 for line in sys.stdin:
   split = line.split(' ')
   time = float(split[0])
-  xaccel = float(split[1])
+  accel = [float(split[1]), float(split[2]), float(split[3])]
+  accel_len = math.sqrt(accel[0]*accel[0] + accel[1]*accel[1] + accel[2]*accel[2])
+  accel_normalized = [accel[0] / accel_len, accel[1] / accel_len, accel[2] / accel_len]
 
   # push new data to the window
-  push_data(time, xaccel)
+  push_data(time, accel_normalized[0])
 
   # expire old data from the window
   expire_data(time)
 
-  mean_xaccel = window_total / len(window_data)
-  print("%f" % (mean_xaccel))
+  mean_zaccel = window_total / len(window_data)
+  print("%f %f" % (time, mean_zaccel))
